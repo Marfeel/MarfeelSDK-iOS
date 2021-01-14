d98b3974e9e51df3e26412a9ffd8c536280a1799
Miguel Santiago Rodríguez <msantiago@atsistemas.com>
2021-01-14 12:03:54 +0100

    Initial Commit

---
+++ CompassSDK/CompassSDK.h
@@ -0,0 +1,18 @@
+//
+//  CompassSDK.h
+//  CompassSDK
+//
+//  Created by  on 14/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CompassSDK.
+FOUNDATION_EXPORT double CompassSDKVersionNumber;
+
+//! Project version string for CompassSDK.
+FOUNDATION_EXPORT const unsigned char CompassSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CompassSDK/PublicHeader.h>
+
+
