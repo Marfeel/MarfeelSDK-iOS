5b181c812bfaa4e66491cdea9fef045ce5ea62ca
Miguel Santiago Rodríguez <msantiago@atsistemas.com>
2022-06-24 17:45:59 +0200

    timestamp without millis

--- CompassSDK/Extensions/Date.swift
+++ CompassSDK/Extensions/Date.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 extension Date {
-    var timeStamp: Int64 {Int64((self.timeIntervalSince1970 * 1000.0).rounded())}
+    var timeStamp: Int64 {Int64((self.timeIntervalSince1970).rounded())}
     
     func adding(minutes: Int) -> Date {
         Calendar.current.date(byAdding: .minute, value: minutes, to: self)!
