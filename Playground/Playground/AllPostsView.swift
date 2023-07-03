f00c2709f2a90653221fb4fad6dd4856da4f17d8
marcmrf <marc.garcia@marfeel.com>
2023-07-03 17:45:54 +0200

    feat: track screen

--- Playground/Playground/AllPostsView.swift
+++ Playground/Playground/AllPostsView.swift
@@ -33,7 +33,7 @@ struct AllPosts: View {
             CompassTracker.shared.setSessionVar(name: "lolo2", value: "lola2")
             CompassTracker.shared.setUserVar(name: "hihi", value: "haha")
             CompassTracker.shared.setUserVar(name: "hihi2", value: "haha2")
-            CompassTracker.shared.trackNewPage(url: URL(string: "https://dev.marfeel.co")!)
+            CompassTracker.shared.trackScreen("ios homepage")
         })
     }
 }
